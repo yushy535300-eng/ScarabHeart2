64b4596fbb94102e9946c4a29e84bca493f6d789
yushy535300-eng <yushy535300@gmail.com>
2026-09-26 07:08:38 +0800

    1231232

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@
 
   const $ = id => document.getElementById(id);
   const log = (...args) => { try { console.log('[ScarabHeart]', ...args); } catch (_) {} };
-  const APP_VERSION = 'v3.02-pro-mobile-login-no-crop';
+  const APP_VERSION = 'v3.03-mobile-login-clean-fit';
   const GAMES = [
     ['golden-seth', '戰神賽特2 覺醒之力', 'media/game2.png'],
     ['egyptian-mythology', '戰神賽特', 'media/game8.png'],
